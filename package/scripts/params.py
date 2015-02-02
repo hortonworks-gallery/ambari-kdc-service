d1e7df6d5ed77b8dc80fc728fa6baf633db7ee8f
Ali Bajwa <abajwa@hortonworks.com>
2015-02-02 02:50:29 -0800

    updated README

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -11,11 +11,3 @@ kdc_domain = config['configurations']['krb5-config']['kdc.domain']
 kdc_admin = config['configurations']['krb5-config']['kdc.admin']
 kdc_adminpassword = config['configurations']['krb5-config']['kdc.adminpassword']
 kdb_password = config['configurations']['krb5-config']['kdb.password']
-
-
-
-   <property>
-    <name>kdc.host</name>
-    <value>sandbox.hortonworks.com</value>
-    <description>Host name where KDC resides e.g. sandbox.hortonworks.com</description>
-  </property> 
