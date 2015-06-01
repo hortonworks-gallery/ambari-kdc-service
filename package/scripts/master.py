985325e261af357b3c2c25a1a7c5cb4dc8d091ac
Ali Bajwa <abajwa@hortonworks.com>
2015-05-31 19:50:42 -0700

    use pid file to monitor status and expose krb5.conf via Ambari conf

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -9,15 +9,15 @@ class Master(Script):
     self.configure(env)
     import params
 
-    Execute('echo clusterHostInfo contents:' +  str(', '.join(params.clusterHostInfo)))
+    #Execute('echo clusterHostInfo contents:' +  str(', '.join(params.clusterHostInfo)))
     
     #e.g. /var/lib/ambari-agent/cache/stacks/HDP/2.2/services/kdc-stack/package
-    service_packagedir = os.path.realpath(__file__).split('/scripts')[0] 
-    Execute('/bin/cp -f '+service_packagedir+'/templates/krb5.conf /etc')
+    #service_packagedir = os.path.realpath(__file__).split('/scripts')[0] 
+    #Execute('/bin/cp -f '+service_packagedir+'/templates/krb5.conf /etc')
 
-    Execute('sed -i "s/kerberos.example.com/'+params.kdc_host+'/g" /etc/krb5.conf')    
-    Execute('sed -i "s/EXAMPLE.COM/'+params.kdc_realm+'/g" /etc/krb5.conf')
-    Execute('sed -i "s/example.com/'+params.kdc_domain+'/g" /etc/krb5.conf')
+    #Execute('sed -i "s/kerberos.example.com/'+params.kdc_host+'/g" /etc/krb5.conf')    
+    #Execute('sed -i "s/EXAMPLE.COM/'+params.kdc_realm+'/g" /etc/krb5.conf')
+    #Execute('sed -i "s/example.com/'+params.kdc_domain+'/g" /etc/krb5.conf')
     
 
     Execute('echo "'+params.kdb_password+'" > passwd.txt')
@@ -40,29 +40,38 @@ class Master(Script):
 
     Execute('echo "*/admin@'+params.kdc_realm+' *" > /var/kerberos/krb5kdc/kadm5.acl')
 
-    #Execute('/etc/rc.d/init.d/krb5kdc restart')
-    #Execute('/etc/rc.d/init.d/kadmin restart')
 
 
-  def configure(self, env):
+  def configure(self, env):    
     import params
+    import status_params    
     env.set_params(params)
+    
+    content=InlineTemplate(status_params.krb5_template_config)
+    File(format("/etc/krb5.conf"), content=content, owner='root',group='root', mode=0644)
+    
 
   def stop(self, env):
     import params
+    import status_params
+    self.configure(env)
+    
     Execute('service krb5kdc stop')
     Execute('service kadmin stop')
           
   def start(self, env):
     import params
+    import status_params
+    self.configure(env)
+    
     Execute('service krb5kdc start')
     Execute('service kadmin start')
-	
+    Execute("service krb5kdc status |  sed 's/.*(pid\s*\(.*\)).*/\1/' > " + status_params.kdc_pidfile)
 
   def status(self, env):
-    import params
-    Execute('service krb5kdc status')
-
+    import status_params
+    env.set_params(status_params)  
+    check_process_status(status_params.kdc_pidfile)  
     
 if __name__ == "__main__":
   Master().execute()
--- package/scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e 
-
-yum install -y nc tigervnc-server tigervnc-server-applet xterm nmap
-
